6f62a802ddfb63554d1546fa96b86c7c56e318aa
kevinrm8 <55388267+kevinrm8@users.noreply.github.com>
2019-10-07 17:39:37 +0200

    Add files via upload

---
+++ Programacion/Boletin1/src/Ejercicio10.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio10 {
+	/* Nombre: Kevin
+	 * Fecha:02/10/19
+	 * Descripcion:  Calculamos el % de niños y niñas en el aula
+	 * 
+	 * */
+
+	public static void main(String[] args) {
+		float niños =21; float niñas = 2; float total=0;
+		total= niños + niñas;
+		System.out.println("Tenemos matriculados "+niños+" niños y "+niñas+" niñas. En total, tenemos mariculados "+total+" alumnos, siendo un "+((niños/total)*100)+ " perteneciente a los niños y "+((niñas/total)*100)+" perteneciente a las niñas.");
+		
+	}
+
+}
