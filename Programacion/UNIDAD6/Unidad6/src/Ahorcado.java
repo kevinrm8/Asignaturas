474028bcb5947a3a77ed12811bcd31cf0ed33fb5
kevinrm8 <kevmar@alu.iesserpis.org>
2019-12-04 00:15:02 +0100

    Ahorcado

--- Programacion/UNIDAD6/Unidad6/src/Ahorcado.java
+++ Programacion/UNIDAD6/Unidad6/src/Ahorcado.java
@@ -6,59 +6,88 @@ public class Ahorcado {
 	
 	public static void main(String[] args) {
 
-		String cadena;
-		String[] array;
-		char letra;
-		String buscar;
+		String palabra;
+		int intentos=0,aciertos=0,INTENTOS_TOTALES=5;
+		char respuesta;
+		char [] separada;
+		char [] turespuesta;
+		char caracter;
+		char []copia;
 		
-		System.out.println("Palabra:");
-		cadena = scanner.nextLine();
+//		System.out.println("Palabra:");
+//		palabra = scanner.nextLine();
 		
-		array = new String [cadena.length()];
-
-		Secreta(cadena,array); // RELLENO PALABRA CON -
+		do {
+			System.out.println("Palabra:");
+			palabra = scanner.nextLine();
+			
+			separada = caracter(palabra);
+			copia = caracter(palabra);
+			
+			turespuesta = new char [separada.length]; // Char de respuesta relleno con - 
+			for(int i=0;i<separada.length;i++) {
+				turespuesta[i] ='-';
+			}
+			
+			while(intentos< INTENTOS_TOTALES && aciertos != turespuesta.length) {
+				Palabraoculta(turespuesta);
+			
+				System.out.print("\nIntroduce una letra:");
+				caracter = scanner.next().charAt(0);
+			
+				for(int i=0;i<separada.length;i++) {
+					if(caracter == separada[i]) {
+						turespuesta[i]=separada[i];
+						separada[i] = ' ';
+						aciertos++;
+					}
+				}
+					intentos++;
+			}
+			
+			if(aciertos == turespuesta.length) {
+			System.out.print("\nFelicidades, has acertado la palabra " );
+			Palabraoculta(turespuesta);
+			}else { 
+				System.out.print("Has fallada y no quedan intentos, la palabra era: ");
+				for(int i=0;i<separada.length;i++)
+					System.out.print(copia[i]+" ");
+			}
+			
+			respuesta = pregunta("\nQuiere volver a jugar?",scanner);
+			
+			//RESET DE ACIERTOS E INTENTOS
+			aciertos =0;
+			intentos=0;
+			
+		}while(respuesta != 'n');
 		
 		
-		
-		System.out.println("Dime la letra a buscar:");
-		buscar = scanner.nextLine();
-		
-		letra = buscar.charAt(0);
-		
-		
-//		for(int i=0;i<cadena.length();i++) {
-//			if(cadena.charAt(i)==letra) {
-//				array[i]=buscar;
-//			}
-//			System.out.print(array[i]);
-//		}
-	
-		
-		
-		}
-		
-		
-
-		
-//	String transmision;
-//	
-//	System.out.println("Introduzca la transmision a cifrar:");
-//	transmision = scanner.nextLine();
-//	
-//	if(transmision.indexOf("el area 51") == -1) 
-//		System.out.println("El area 51 no aparece en la retansmision");	
-//	else {
-//		transmision = transmision.replaceAll("el area 51", "Murcia");
-//		System.out.println(transmision);		
-//	}
-
-public static void Secreta(String cadena,String[] array) {
-	
-	for(int i=0;i<cadena.length();i++) {
-	array[i] ="-" ;
-	System.out.print(array[i]);
-}		
 	}
+	
+	// Separo la palabra en caracteres y lo guardo en un char
+	public static char[] caracter(String palabra) {
+		char [] letra;
+		letra = new char[palabra.length()];
+		for(int i=0;i<palabra.length();i++)
+			letra [i] = palabra.charAt(i);
+		return letra;
+	}
+	public static void Palabraoculta(char[] turespuesta) {
+		for(int i=0;i<turespuesta.length;i++)
+			System.out.print(turespuesta[i]+" ");
+	}
+	
+	 public static char pregunta(String men, Scanner scanner) {
+	        char resp;
+	        System.out.println(men + " (s/n)");
+	        resp = scanner.next().toLowerCase().charAt(0);
+	        while (resp != 's' && resp != 'n') {
+	            System.out.println("Error! solo se admite S o N");
+	            resp = scanner.next().toLowerCase().charAt(0);
+	        }
+	        return resp;
+	    }
 }
 
 
