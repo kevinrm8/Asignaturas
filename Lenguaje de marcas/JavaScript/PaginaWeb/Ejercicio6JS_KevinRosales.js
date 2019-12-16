ee2f70895c323e9ac215a2a9fbb17c8ff4856668
kevinrm8 <kevmar@alu.iesserpis.org>
2019-12-16 13:58:17 +0100

    LM

---
+++ Lenguaje de marcas/JavaScript/PaginaWeb/Ejercicio6JS_KevinRosales.js
@@ -0,0 +1,15 @@
+
+
+
+function Diabetico(){ 
+    
+    var r = confirm("\t\t¡ATENCION!\n Si eres diabetico te aconsejamos no entrar en la pagina.")
+   if(r == false){
+       confirm("Entro bajo mi responsabilidad");}
+        else 
+            window.replace("Diabetico.html");
+           
+}
+
+      
+
