59210fbc705410fd4294f079a8989ff84e95fe30
kevinrm8 <kevmar@alu.iesserpis.org>
2019-12-18 00:42:57 +0100

    JS

---
+++ Lenguaje de marcas/JavaScript/Calculadora/js/script.js
@@ -0,0 +1,125 @@
+document.getElementById("num1").addEventListener("click",num1);
+document.getElementById("num2").addEventListener("click",num2);
+document.getElementById("num3").addEventListener("click",n3);
+document.getElementById("num4").addEventListener("click",n4);
+document.getElementById("num5").addEventListener("click",n5);
+document.getElementById("num6").addEventListener("click",n6);
+document.getElementById("num7").addEventListener("click",n7);
+document.getElementById("num8").addEventListener("click",n8);
+document.getElementById("num9").addEventListener("click",n9);
+document.getElementById("num0").addEventListener("click",n0);
+document.getElementById("suma").addEventListener("click",o1);
+document.getElementById("resta").addEventListener("click",o2);
+document.getElementById("div").addEventListener("click",o3);
+document.getElementById("mult").addEventListener("click",o4);
+document.getElementById("reset").addEventListener("click",reset);
+document.getElementById("igual").addEventListener("click",Result);
+
+function num1() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num1").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+function num2() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num2").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+function n3() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num3").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+function n4() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num4").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+function n5() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num5").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+function n6() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num6").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+function n7() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num7").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+function n8() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num8").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+function n9() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num9").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+function n0() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("num0").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function o1() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("suma").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function o2() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("resta").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function o3() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("div").innerHTML;
+	document.getElementById('resultado').innerHTML = actual + sumado
+}
+
+function o4() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("mult").innerHTML;
+	document.getElementById('resultado').innerHTML = actual +  sumado
+}
+function reset() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let sumado = document.getElementById("coma").innerHTML;
+	document.getElementById('resultado').innerHTML = "";
+}
+
+function Result() {
+	let actual = document.getElementById('resultado').innerHTML;
+	let suma = actual.indexOf("+");
+	let resta = actual.indexOf("-");
+	let div = actual.indexOf("÷");
+	let mult = actual.indexOf("x");
+    
+	if (suma !== -1) {
+		arr = actual.split("+",2);
+		res = parseInt(arr[0]) + parseInt(arr[1]);
+		document.getElementById("resultado").innerHTML = res;
+	} else if (resta !== -1) {
+		arr = actual.split("-",2);
+		res = arr[0] - arr[1];
+		document.getElementById("resultado").innerHTML = res;
+		
+	} else if (div !== -1) {
+		arr = actual.split("÷",2);
+		res = arr[0] / arr[1];
+		document.getElementById("resultado").innerHTML = res;
+		
+	} else if (mult !== -1) {
+		arr = actual.split("x",2);
+		res = arr[0] * arr[1];
+		document.getElementById("resultado").innerHTML = res;
+		
+	}
+}
--- Lenguaje de marcas/JavaScript/PaginaWeb/Ejercicio6JS_KevinRosales.js
+++ Lenguaje de marcas/JavaScript/PaginaWeb/Ejercicio6JS_KevinRosales.js
@@ -11,11 +11,61 @@ function Diabetico(){  //Pedir acceso a la pagina
         }else 
             alert("Bienvenido a la pagina!");    
 }
-var historia = 0;
 
-function Mostrar(){
-    hisotria++;
-   window.alert(""+historia);
+    //HISTORIA PAGINA 2
+var x;
+function siguiente(){ // CADA X SEGUNDOS SE MUESTRA ALGO DE LA HISTORIA
+    document.getElementById("descubre").innerHTML = "";
+    
+    x = setTimeout(function(){titulo("");},1000);
+    
+    x = setTimeout(function(){Anyo92("");},4000);
+    x = setTimeout(function(){hist92("");},5000);
+    
+    x = setTimeout(function(){Anyo98("");},8000);
+    x = setTimeout(function(){hist98("");},9000);
+    
+    x = setTimeout(function(){Anyo03("");},12000);
+    x = setTimeout(function(){hist03("");},13000);
+    
+    x = setTimeout(function(){Anyo10("");},16000);
+    x = setTimeout(function(){hist10("");},17000);
+    
+    x = setTimeout(function(){Anyo17("");},20000);
+    x = setTimeout(function(){hist17("");},21000);
     
 }
-
+// FUNCIONES PARA QUE SE MUESTRO AL PASO DE LOS SEGUNDOS LO QUE QUEREMOS
+function titulo(){
+    document.getElementById("titulo").innerHTML = "HISTORIA DE LA EMPRESA";
+}
+function Anyo92(){
+    document.getElementById("1992").innerHTML = "1992";
+}
+function hist92(){
+    document.getElementById("hist92").innerHTML = "La familia fundadora Rosales decide emprender una aventura empresarial y nace Chucherias S.L.";
+}
+function Anyo98(){
+    document.getElementById("1998").innerHTML = "1998";
+}
+function hist98(){
+    document.getElementById("hist98").innerHTML = "Nuestro afan por cruzar fronteras nos llevo a ser la primera empresa española en participar en la Feria ISM de Alemania.";
+}
+function Anyo03(){
+    document.getElementById("2003").innerHTML = "2003";
+}
+function hist03(){
+    document.getElementById("hist03").innerHTML = "Fuimos los primeros en fabricar regaliz en continuo.";
+}
+function Anyo10(){
+    document.getElementById("2010").innerHTML = "2010";
+}
+function hist10(){
+    document.getElementById("hist10").innerHTML = "Necesitabamos mas espacio, estamos en pleno crecimiento. Pusimos en marcha un segundo centro productivo.";
+}
+function Anyo17(){
+    document.getElementById("2017").innerHTML = "2017";
+}
+function hist17(){
+    document.getElementById("hist17").innerHTML = "Seguimos creciendo y ampliamos nuestras instalaciones con un centro logistico.";
+}
