76bb7d635835e8dc57e58051e953b228dcdbc362
kevinrm8 <kevmar@alu.iesserpis.org>
2020-02-11 13:04:06 +0100

    PG

--- Entorno Desarrollo/Projects/Calc/Calc/MainWindow.cs
+++ Entorno Desarrollo/Projects/Calc/Calc/MainWindow.cs
@@ -6,8 +6,8 @@ public partial class MainWindow : Gtk.Window
     //Cuando le damos a operacion +-*/ guardamos operando1, borramos el display
     //entonces escribimos segundo numero, al darle al = lo guardamos en oeprando2
     //Con switch case sabemos que operacion tenemos que hacer y mostramos resultado 
-    private double operando1,operando2;
-    private String operacion;
+    public double operando1,operando2;
+    public String operacion;
 
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
@@ -22,13 +22,17 @@ public partial class MainWindow : Gtk.Window
 
     protected void OnClickedpunto(object sender, EventArgs e)
     {
-        String Display = Pantalla.Text.ToString();
-        Pantalla.DeleteText(0, Pantalla.Text.Length);
-        Pantalla.InsertText(Display + ".");
+        if (Pantalla.Text.IndexOf(",") < 0) {
+            String Display = Pantalla.Text;
+            Pantalla.DeleteText(0, Pantalla.Text.Length);
+            Pantalla.InsertText(Display + ",");
+        }
+
     }
 
     protected void OnClicked0(object sender, EventArgs e)
     {
+
        String Display = Pantalla.Text.ToString();
         Pantalla.DeleteText(0,Pantalla.Text.Length);
         Pantalla.InsertText(Display + "0");
@@ -37,17 +41,21 @@ public partial class MainWindow : Gtk.Window
     protected void OnClickedIgual(object sender, EventArgs e)
     {
         String Display = Pantalla.Text.ToString();
+        
+        operando2 = double.Parse(Pantalla.Text);
         Pantalla.DeleteText(0, Pantalla.Text.Length);
-        Pantalla.InsertText(Display + "=");
+        calcula();
+
     }
 
     protected void OnClickedSuma(object sender, EventArgs e)
     {
-        String Display = Pantalla.Text.ToString();
-        Pantalla.DeleteText(0, Pantalla.Text.Length);
-        Pantalla.InsertText(Display + "+");
+        String Display = Pantalla.Text;
         operacion = "+";
         //Pasar el texto a int para poder trabajar con el
+        operando1 = double.Parse(Pantalla.Text);
+        // Borramos lo que hay en pantalla
+        Pantalla.DeleteText(0, Pantalla.Text.Length); 
     }
 
     protected void OnClicked1(object sender, EventArgs e)
@@ -74,8 +82,13 @@ public partial class MainWindow : Gtk.Window
     protected void OnClickedResta(object sender, EventArgs e)
     {
         String Display = Pantalla.Text.ToString();
+        operacion = "-";
+        //Pasar el texto a int para poder trabajar con el
+        operando1 = double.Parse(Pantalla.Text);
+        // Borramos lo que hay en pantalla
         Pantalla.DeleteText(0, Pantalla.Text.Length);
-        Pantalla.InsertText(Display + "-");
+
+
     }
 
     protected void OnClicked4(object sender, EventArgs e)
@@ -102,8 +115,12 @@ public partial class MainWindow : Gtk.Window
     protected void OnClickedMulti(object sender, EventArgs e)
     {
         String Display = Pantalla.Text.ToString();
+        operacion = "*";        
+        //Pasar el texto a int para poder trabajar con el
+        operando1 = double.Parse(Pantalla.Text);
+        // Borramos lo que hay en pantalla
         Pantalla.DeleteText(0, Pantalla.Text.Length);
-        Pantalla.InsertText(Display + "*");
+
     }
 
 
@@ -131,8 +148,11 @@ public partial class MainWindow : Gtk.Window
     protected void OnClickedDivision(object sender, EventArgs e)
     {
         String Display = Pantalla.Text.ToString();
+        operacion = "/";
+        //Pasar el texto a int para poder trabajar con el
+        operando1 = double.Parse(Pantalla.Text);
+        // Borramos lo que hay en pantalla
         Pantalla.DeleteText(0, Pantalla.Text.Length);
-        Pantalla.InsertText(Display + "/");
     }
 
     protected void OnClickedReset(object sender, EventArgs e)
@@ -150,12 +170,28 @@ public partial class MainWindow : Gtk.Window
         Pantalla.DeleteText((Pantalla.Text.Length-1), Pantalla.Text.Length);
     }
 
-    public void calcula (String operacion,int operando1)
+    public void calcula ()
     {
         double resultado;
         switch (operacion) {
-            case "+": resultado = operando1 + operando2;
+            case "+":
+             resultado = operando1 + operando2;
+             Pantalla.Text = resultado.ToString();
+             break;
+            
+            case "-":
+            resultado = operando1 - operando2;
+            Pantalla.Text = resultado.ToString();
             break;
+
+            case "*":
+                resultado = operando1 * operando2;
+                Pantalla.Text = resultado.ToString();
+                break;
+            case "/":
+                resultado = operando1 / operando2;
+                Pantalla.Text = resultado.ToString();
+                break;
         }
     }
 }
