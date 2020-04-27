094d3e816cc7f1d540c4990efc0560776901548c
kevinrm8 <kevinmar@alu.iesserpis.org>
2020-04-27 13:35:27 +0200

    TODO

---
+++ Programacion/UNIDAD11/src/Ejer122/Persona.java
@@ -0,0 +1,27 @@
+package Ejer122;
+
+public class Persona {
+
+	private String Nombre,DNI,lugar_de_residencia;
+	private int Edad;
+	
+	public Persona(String Nombre,String DNI, String lugar_de_residencia,int Edad){
+		this.DNI = DNI;
+		this.Nombre = Nombre;
+		this.lugar_de_residencia = lugar_de_residencia;
+		this.Edad = Edad;
+	}
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	public String getDni() {
+		return DNI;
+	}
+	public String getResidencia() {
+		return lugar_de_residencia;
+	}
+	public int getEdad() {
+		return Edad;
+	}
+}
